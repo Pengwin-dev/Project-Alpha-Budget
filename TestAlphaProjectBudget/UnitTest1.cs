5e2e115103257734b2608909baa81efcf3ef77d9
sabrina <Sabrina.Rodriguez@jala.university>
2023-02-08 15:15:17 -0300

    agregar test para false

--- TestAlphaProjectBudget/UnitTest1.cs
+++ TestAlphaProjectBudget/UnitTest1.cs
@@ -25,4 +25,21 @@ public class UnitTest1
         // Assert
         Assert.IsTrue(result);
     }
+
+    [TestMethod]
+    public void TestRegisterProgramReturnFalse()
+    {
+        // Arrange
+        DateTime firstDate = DateTime.Parse("10/05/2023");
+        DateTime endDate = DateTime.Parse("28/05/2023");
+        Programm program = new Programm("Test", "", firstDate, endDate);
+
+        ProgramController programController = new ProgramController();
+
+        // Act
+        bool result = programController.RegisterProgram(program);
+        // Assert
+        Assert.IsFalse(result);
+    }
+
 }
